7422c12eb194a14cbacbb0de7f42159c2e315a72
julioct99 <jcasallot01@gmail.com>
2021-03-21 23:23:40 +0100

    Audio: Add miliseconds to time display

--- pages/audio/index.js
+++ pages/audio/index.js
@@ -153,4 +153,4 @@ function handleSoundLoaded() {
 }
 
 const toTimeString = (seconds) =>
-  new Date(seconds * 1000).toISOString().substr(11, 8);
+  new Date(seconds * 1000).toISOString().substr(11, 12);
