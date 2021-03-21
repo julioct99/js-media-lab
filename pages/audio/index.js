0987dbce96764e075744cf3d714b328964a816c4
julioct99 <jcasallot01@gmail.com>
2021-03-21 18:00:10 +0100

    Audio: More controls + refactor

--- pages/audio/index.js
+++ pages/audio/index.js
@@ -1,20 +1,25 @@
 const playPauseBtn = document.querySelector('#play-pause-btn');
+const playbackRateBtn = document.querySelector('#playback-rate-btn');
+const playbackRateDisplay = document.querySelector('#playback-rate-display');
+const loopBtn = document.querySelector('#loop-btn');
 const volumeInput = document.querySelector('#volume-input');
 const fileInput = document.querySelector('#file-input');
 const progressValue = document.querySelector('.progress-value');
 const progressInput = document.querySelector('#progress-input');
 const spinner = document.querySelector('.lds-ring');
 
-window.URL = window.URL || window.webkitURL;
+let progressInputIsActive = false;
+let progressInterval = null;
+const rates = [0.5, 1.0, 1.5, 2.0];
 
 const audioFile = {
   file: null,
   sound: null,
   url: '',
   loaded: false,
+  loop: false,
+  rate: rates[1],
 };
-
-let progressInterval = null;
 
 fileInput.addEventListener('change', () => {
   audioFile.file = fileInput.files[0];
@@ -22,7 +27,8 @@ fileInput.addEventListener('change', () => {
 
   playPauseBtn.disabled = true;
   progressValue.style.width = '0%';
-  displayLoadingSpinner();
+  progressInput.disabled = true;
+  spinner.style.display = 'inline-block';
 
   // type -> 'audio/wav', 'audio/mp3' ...
   let format = [audioFile.file.type.split('/')[1]];
@@ -31,20 +37,12 @@ fileInput.addEventListener('change', () => {
   audioFile.sound?.stop();
   audioFile.sound = new Howl({
     src: [audioFile.url],
+    rate: audioFile.rate,
     format,
   });
-
   Howler.volume(volumeInput.value);
 
-  audioFile.sound.on('load', () => {
-    audioFile.loaded = true;
-    playPauseBtn.disabled = false;
-    progressInput.style.cursor = 'pointer';
-
-    updatePlayButton();
-    hideLoadingSpinner();
-  });
-
+  audioFile.sound.on('load', handleSoundLoaded);
   audioFile.sound.on('end', updatePlayButton);
 
   clearInterval(progressInterval);
@@ -56,7 +54,22 @@ document.addEventListener('keypress', (event) => {
   if (key === ' ') pressPlayPauseButton();
 });
 
+playbackRateBtn.addEventListener('click', () => {
+  const currentRateIndex = rates.indexOf(audioFile.rate);
+  const newRateIndex = (currentRateIndex + 1) % rates.length;
+  const rate = rates[newRateIndex];
+  audioFile.rate = rate;
+  audioFile.sound?.rate(rate);
+  playbackRateDisplay.textContent = rate % 1 !== 0 ? rate : rate + '.0';
+});
+
 playPauseBtn.addEventListener('click', pressPlayPauseButton);
+
+loopBtn.addEventListener('click', () => {
+  audioFile.loop = !audioFile.loop;
+  audioFile.sound.loop(audioFile.loop);
+  loopBtn.classList.toggle('active');
+});
 
 volumeInput.addEventListener('input', (event) => {
   const volume = event.target.value;
@@ -65,66 +78,61 @@ volumeInput.addEventListener('input', (event) => {
 
 progressInput.addEventListener('input', (event) => {
   if (!audioFile.sound) return;
+  progressInputIsActive = true;
+  const percentProgress = event.target.value * 100;
+  setAudioProgress(percentProgress);
+});
 
-  if (!audioFile.sound.playing()) {
-    const percentProgress = event.target.value * 100;
-    progressValue.style.transition = 'none';
-    progressValue.style.width = percentProgress + '%';
+progressInput.addEventListener('change', (event) => {
+  progressInputIsActive = false;
 
-    const duration = audioFile.sound.duration();
-    const moveTo = duration * (percentProgress / 100);
-
-    audioFile.sound.seek(moveTo);
-  }
+  const percentProgress = event.target.value * 100;
+  setAudioProgress(percentProgress);
+  const duration = audioFile.sound.duration();
+  const moveTo = duration * (percentProgress / 100);
+  audioFile.sound.seek(moveTo);
 });
 
 function pressPlayPauseButton() {
   if (!audioFile.loaded) return;
 
   if (audioFile.sound?.playing()) {
-    handleAudioStop();
+    audioFile.sound.pause();
   } else {
-    handleAudioPlay();
+    audioFile.sound.play();
   }
   updatePlayButton();
 }
 
-function handleAudioStop() {
-  audioFile.sound?.pause();
-  progressValue.style.background = '#4cbb17';
-  progressInput.style.cursor = 'pointer';
-}
-
-function handleAudioPlay() {
-  audioFile.sound?.play();
-  progressValue.style.transition = 'all 0.25s linear';
-  progressValue.style.background = '#007bff';
-  progressInput.style.cursor = 'not-allowed';
-}
-
 function updatePlayButton() {
-  console.log(audioFile.sound.playing());
+  progressValue.style.transition = 'all 0.25s linear';
+  const className = 'material-icons icon-lg';
+
   if (audioFile.sound && audioFile.sound.playing()) {
-    playPauseBtn.innerHTML = `<span class="material-icons"> pause </span>`;
+    playPauseBtn.innerHTML = `<span class="${className}"> pause </span>`;
   } else {
-    playPauseBtn.innerHTML = `<span class="material-icons"> play_arrow </span>`;
+    playPauseBtn.innerHTML = `<span class="${className}"> play_arrow </span>`;
   }
 }
 
 function updateAudioProgress() {
-  if (audioFile.sound && audioFile.sound.playing()) {
+  if (!progressInputIsActive && audioFile.sound && audioFile.sound.playing()) {
     const currentTime = audioFile.sound.seek();
     const duration = audioFile.sound.duration();
     const width = (currentTime / duration) * 100;
-
     progressValue.style.width = width.toFixed(2) + '%';
   }
 }
 
-function displayLoadingSpinner() {
-  spinner.style.display = 'inline-block';
+function setAudioProgress(percentProgress) {
+  progressValue.style.transition = 'none';
+  progressValue.style.width = percentProgress + '%';
 }
 
-function hideLoadingSpinner() {
+function handleSoundLoaded() {
+  audioFile.loaded = true;
+  playPauseBtn.disabled = false;
+  progressInput.disabled = false;
   spinner.style.display = 'none';
+  updatePlayButton();
 }
